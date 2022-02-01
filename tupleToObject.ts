1479bac1d52abd588eb34e258f9d54a4ea1791ca
savely <avelikodny@gmail.com>
2022-02-01 19:06:25 +0200

    #11 Tuple to Object - accepted

---
+++ tupleToObject.ts
@@ -0,0 +1,17 @@
+/*
+#11 Tuple to Object
+
+Give an array, transform into an object type and the key/value must in the given array.
+
+For example
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+*/
+
+type TupleToObject<T extends readonly any[]> = {
+
+    [K in T[number]] : T[K]
+}
