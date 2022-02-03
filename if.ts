7d0b9d1c25e05841e873ae0be78f0f2e4ce53402
savely <avelikodny@gmail.com>
2022-02-03 19:36:50 +0200

    #268 If  - with Falsy type, not accepted

---
+++ if.ts
@@ -0,0 +1,28 @@
+/*
+#268 If
+
+ Implement a utils `If` which accepts condition `C`, a truthy return type `T`, and a falsy return type `F`. `C` is expected to be either `true` or `false` while `T` and `F` can be any type.
+  
+  For example:
+  
+  ```ts
+  type A = If<true, 'a', 'b'>  // expected to be 'a'
+  type B = If<false, 'a', 'b'> // expected to be 'b'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/268
+
+  
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+]
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
+
+*/
+
+type Falsy = "" | null | undefined | false;
+
+type If<C, T, F> = C extends Falsy ? F : T;
